05ba9eb765f2dc9a2d5f5122c3d7a66b7d55598f
Xinya <44987966+Qiuben@users.noreply.github.com>
2026-01-04 20:54:39 +0800

    Add files via upload

---
+++ models/detection/recurrent_backbone/__init__.py
@@ -0,0 +1,12 @@
+from omegaconf import DictConfig
+
+# from .maxvit_rnn import RNNDetector as MaxViTRNNDetector
+from .hourglass_rnn import RNNDetector as MaxViTRNNDetector
+
+
+def build_recurrent_backbone(backbone_cfg: DictConfig):
+    name = backbone_cfg.name
+    if name == 'MaxViTRNN':
+        return MaxViTRNNDetector(backbone_cfg)
+    else:
+        raise NotImplementedError
